b87e9347377c657d1ad923382c10da2cff6b1a16
Huyvictory <Huyvictory@HUYVICTORY-PC>
2023-11-27 23:22:24 +0700

    Add project files.

---
+++ EmployeManagement.Test/EmployeeFactoryTests.cs
@@ -0,0 +1,33 @@
+﻿using EmployeeManagement.Business;
+using EmployeeManagement.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeManagement.Test
+{
+    public class EmployeeFactoryTests
+    {
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_IsInternalEmployee()
+        {
+            var employeeFactory = new EmployeeFactory();
+
+            var isInternalEmployee = (InternalEmployee)employeeFactory.CreateEmployee("Huy", "Vo");
+
+            Assert.NotNull(isInternalEmployee);
+        }
+
+        [Fact]
+        public void CreateEmployee_EnsureBaseSalary2500()
+        {
+            var employeeFactory = new EmployeeFactory();
+
+            var InternalEmployee = (InternalEmployee)employeeFactory.CreateEmployee("Huy", "Vo");
+
+            Assert.Equal(2500, InternalEmployee.Salary);
+        }
+    }
+}
