2b9aaf985c42c38b83ef457513dbe4aa69917841
Huyvictory <vominhhuy008@gmail.com>
2023-12-02 23:31:22 +0700

    Test by using Theory with data provided from inline, member, class, strong typed TheoryData, from external source like spreadsheet file

--- EmployeManagement.Test/DataDrivenEmployeeServiceTests.cs
+++ EmployeManagement.Test/DataDrivenEmployeeServiceTests.cs
@@ -2,6 +2,7 @@
 using EmployeeManagement.Business.Exceptions;
 using EmployeeManagement.DataAccess.Entities;
 using EmployeManagement.Test.Fixtures;
+using EmployeManagement.Test.TestData;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +15,42 @@ namespace EmployeManagement.Test
     public class DataDrivenEmployeeServiceTests //: IClassFixture<EmployeeServiceFixture>
     {
         private readonly EmployeeServiceFixture _employeeServiceFixture;
+
+        public static IEnumerable<object[]> ExampleTestDataForGiveRaise_WithPropery
+        {
+            get
+            {
+                return new List<object[]>
+                {
+                    new object[] { 100, true },
+                    new object[] { 200, false }
+                };
+            }
+        }
+
+        public static TheoryData<int, bool> StronglyTypedExampleTestDataForGiveRaise_WithPropery
+        {
+            get
+            {
+                return new TheoryData<int, bool>
+                {
+                     { 100, true },
+                     { 200, false }
+                };
+            }
+        }
+
+        public static IEnumerable<object[]> ExampleTestDataForGiveRaise_WithMethod(
+            int testDataInstancesToProvide)
+        {
+            var testData = new List<object[]>
+            {
+                new object[] { 100, true },
+                new object[] { 200, false }
+            };
+
+            return testData.Take(testDataInstancesToProvide);
+        }
 
         public DataDrivenEmployeeServiceTests(EmployeeServiceFixture employeeServiceFixture)
         {
@@ -45,6 +82,41 @@ namespace EmployeManagement.Test
 
             // Assert
             Assert.False(internalEmployee.MinimumRaiseGiven);
+        }
+
+        [Theory]
+        [InlineData("37e03ca7-c730-4351-834c-b66f280cdb01")]
+        [InlineData("1fd115cf-f44c-4982-86bc-a8fe2e4ff83e")]
+        public void CreateInternalEmployee_InternalEmployeeCreated_MustHaveAttendedObligatoryClassWithSpecificGuid
+            (Guid courseId)
+        {
+            // Arrange
+            // Act
+            var internalEmployee = _employeeServiceFixture.EmployeeService.CreateInternalEmployee("Huy", "Vo");
+
+            // Assert
+            Assert.Contains(internalEmployee.AttendedCourses,
+                course => course.Id == courseId);
+        }
+
+        [Theory]
+        //[MemberData(
+        //    nameof(ExampleTestDataForGiveRaise_WithMethod),
+        //    2,
+        //    MemberType = typeof(DataDrivenEmployeeServiceTests))]
+        //[ClassData(typeof(EmployeeServiceTestData))]
+        //[ClassData(typeof(StronglyTypedEmployeeServiceTestData))]
+        [ClassData(typeof(StronglyTypedEmployeServiceTestData_FromFile))]
+        public async Task GiveRaise_RaiseGiven_EmployeeMinimumRaiseGivenMatchesValue(int raiseGiven, bool expectedValueForMinimumRaiseGiven)
+        {
+            // Arrange
+            var internalEmployee = new InternalEmployee("Huy", "Vo", 5, 3000, false, 1);
+
+            // Act
+            await _employeeServiceFixture.EmployeeService.GiveRaiseAsync(internalEmployee, raiseGiven);
+
+            // Assert
+            Assert.Equal(expectedValueForMinimumRaiseGiven, internalEmployee.MinimumRaiseGiven);
         }
     }
 }
---
+++ EmployeManagement.Test/TestData/StronglyTypedEmployeServiceTestData_FromFile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeManagement.Test.TestData
+{
+    public class StronglyTypedEmployeServiceTestData_FromFile : TheoryData<int, bool>
+    {
+        public StronglyTypedEmployeServiceTestData_FromFile()
+        {
+            var testDataLines = File.ReadAllLines("TestData/EmployeeServiceTestData.csv");
+            foreach (var line in testDataLines)
+            {
+                // Split the string
+                var splittedString = line.Split(',');
+                // Try parsing
+                if (int.TryParse(splittedString[0], out int raise) && bool.TryParse(splittedString[1], out bool minimumRaiseGiven))
+                {
+                    // add test data
+                    Add(raise, minimumRaiseGiven);
+                }
+            }
+        }
+    }
+}
