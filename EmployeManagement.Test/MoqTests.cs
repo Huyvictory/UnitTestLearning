4aaa51f904719cca591989b53f6c3578a7150a8d
Huyvictory <vominhhuy008@gmail.com>
2023-12-03 18:29:27 +0700

    Isolate unit test with SQLite in-memory mode, httpClient. Using Moq to set up tests and return mock data for testing.

---
+++ EmployeManagement.Test/MoqTests.cs
@@ -0,0 +1,99 @@
+﻿using EmployeeManagement.Business;
+using EmployeeManagement.DataAccess.Entities;
+using EmployeeManagement.DataAccess.Services;
+using EmployeeManagement.Services.Test;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeManagement.Test
+{
+    public class MoqTests
+    {
+        [Fact]
+        public void FetchInternalEmployee_EmployeeFetched_SuggestedBonusMustBeCalculatedCorrectly_MoqInterface()
+        {
+            // Arrange
+            //var employeeManagementTestDataRepository = new EmployeeManagementTestDataRepository();
+            var employeeManagementTestDataRepositoryMock = new Mock<IEmployeeManagementRepository>();
+
+            employeeManagementTestDataRepositoryMock
+                .Setup(m => m.GetInternalEmployee(It.IsAny<Guid>()))
+                .Returns(new InternalEmployee("Tony", "Hall", 2, 2500, false, 2)
+                {
+                    AttendedCourses = new List<Course>
+                    {
+                        new Course("A course"),
+                        new Course("Another course")
+                    }
+                });
+
+            //var employeeFactory = new EmployeeFactory();
+            var employeeFactoryMock = new Mock<EmployeeFactory>();
+            var employeeService = new EmployeeService(
+                employeeManagementTestDataRepositoryMock.Object, employeeFactoryMock.Object);
+
+            // Act
+            var employee = employeeService.FetchInternalEmployee(
+                Guid.Empty);
+
+            // Assert
+            Assert.Equal(400, employee.SuggestedBonus);
+        }
+
+        [Fact]
+        public void CreateInternalEmployee_InternalEmployeeCreated_SuggestedBonusMustBeCalculatedCorrectly_MoqFactory()
+        {
+            // Arrange
+            var employeeManagementTestDataRepository = new EmployeeManagementTestDataRepository();
+            var employeeFactoryMock = new Mock<EmployeeFactory>();
+            employeeFactoryMock.Setup(
+                m => m.CreateEmployee(It.IsAny<string>(), It.IsAny<string>(), null, false))
+                .Returns(new InternalEmployee("Huy", "Vo", 5, 3000, false, 1));
+            var employeeService = new EmployeeService(
+                employeeManagementTestDataRepository, employeeFactoryMock.Object);
+
+            decimal suggestedBonus = 1000;
+
+            // Act
+            var employee = employeeService.CreateInternalEmployee("Huy", "Vo");
+
+            // Assert
+            Assert.Equal(suggestedBonus, employee.SuggestedBonus);
+        }
+
+        [Fact]
+        public async Task FetchInternalEmployee_EmployeeFetched_SuggestedBonusMustBeCalculatedCorrectly_MoqAsync()
+        {
+            // Arrange
+            //var employeeManagementTestDataRepository = new EmployeeManagementTestDataRepository();
+            var employeeManagementTestDataRepositoryMock = new Mock<IEmployeeManagementRepository>();
+
+            employeeManagementTestDataRepositoryMock
+                .Setup(m => m.GetInternalEmployeeAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(new InternalEmployee("Tony", "Hall", 2, 2500, false, 2)
+                {
+                    AttendedCourses = new List<Course>
+                    {
+                        new Course("A course"),
+                        new Course("Another course")
+                    }
+                });
+
+            //var employeeFactory = new EmployeeFactory();
+            var employeeFactoryMock = new Mock<EmployeeFactory>();
+            var employeeService = new EmployeeService(
+                employeeManagementTestDataRepositoryMock.Object, employeeFactoryMock.Object);
+
+            // Act
+            var employee = await employeeService.FetchInternalEmployeeAsync(
+                Guid.Empty);
+
+            // Assert
+            Assert.Equal(400, employee.SuggestedBonus);
+        }
+    }
+}
---
+++ EmployeManagement.Test/TestIsolationApproachesTests.cs
@@ -0,0 +1,77 @@
+﻿using EmployeeManagement.Business;
+using EmployeeManagement.DataAccess.DbContexts;
+using EmployeeManagement.DataAccess.Entities;
+using EmployeeManagement.DataAccess.Services;
+using EmployeeManagement.Services.Test;
+using EmployeManagement.Test.HttpMessageHandlers;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Sdk;
+
+namespace EmployeManagement.Test
+{
+    public class TestIsolationApproachesTests
+    {
+        [Fact]
+        public async Task AttendCourseAsync_CourseAttended_SuggestedBonusMustCorrectlyReCalculated()
+        {
+            // Arrange
+            var connection = new SqliteConnection("Data Source=:memory:");
+            connection.Open();
+
+            var optionsBuilder = new DbContextOptionsBuilder<EmployeeDbContext>().UseSqlite(connection);
+
+            var dbContextSqlLite = new EmployeeDbContext(optionsBuilder.Options);
+            dbContextSqlLite.Database.Migrate();
+
+            var employeeManagementDataRepository = new EmployeeManagementRepository(dbContextSqlLite);
+
+            var employeeService = new EmployeeService(employeeManagementDataRepository, new EmployeeFactory());
+
+            // Get course from databse - "Dealing with Customers 101"
+            var courseToAttend = await employeeManagementDataRepository
+                .GetCourseAsync(Guid.Parse("844e14ce-c055-49e9-9610-855669c9859b"));
+
+            // Get exisiting employee - "Megan Jones"
+            var internalEmployee = await employeeManagementDataRepository
+                .GetInternalEmployeeAsync(Guid.Parse("72f2f5fe-e50c-4966-8420-d50258aefdcb"));
+
+            if (courseToAttend == null || internalEmployee == null)
+            {
+                throw new XunitException("Arrange the test failed!");
+            }
+
+            // expected suggest bonus after internal employee has attended a new course
+            var expectedSuggestBonus = internalEmployee.YearsInService *
+                (internalEmployee.AttendedCourses.Count + 1) * 100;
+            // Act
+            await employeeService.AttendCourseAsync(internalEmployee, courseToAttend);
+
+            // Assert
+            Assert.Equal(expectedSuggestBonus, internalEmployee.SuggestedBonus);
+        }
+
+        [Fact]
+        public async Task PromoteInternalEmployeeAsync_IsEligible_JobLevelMustBeIncreased()
+        {
+            // Arrange
+            var httpClient = new HttpClient(
+                new TestablePromotionEligibilityHandler(true));
+            var internalEmployee = new InternalEmployee("Huy", "Vo", 5, 3000, false, 1);
+
+            var promotionService = new PromotionService(httpClient,
+                new EmployeeManagementTestDataRepository());
+
+            // Act
+            await promotionService.PromoteInternalEmployeeAsync(internalEmployee);
+
+            // Assert
+            Assert.Equal(2, internalEmployee.JobLevel);
+        }
+    }
+}
